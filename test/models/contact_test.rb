98f9fa4049f33c5756870c1bfb58a161c2360c36
Pablo Marti <pablo.marti.dev@gmail.com>
2020-06-28 15:18:02 -0600

    Update views for displaying donors

--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -4,15 +4,18 @@
 #
 #  id          :bigint           not null, primary key
 #  user_id     :bigint           not null
-#  hospital_id :bigint           not null
+#  hospital_id :integer
 #  first_name  :string
 #  last_name   :string
 #  mobile      :string
-#  status      :integer          default(0)
+#  status      :integer          default("si")
 #  details     :text
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
-#  blood_type  :integer          default(0)
+#  blood_type  :integer          default("A+")
+#  kind        :integer          default("patient")
+#  background  :integer          default("possible")
+#  email       :string           default("")
 #
 require 'test_helper'
 
